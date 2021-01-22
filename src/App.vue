064d8b8cd7b9c8dcc51f98cfa8ac839e1f373b15
Manuel <merodriguez9112@gmail.com>
2021-01-22 15:59:44 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<h1>
+  Hello Word
+</h1>
+</template>
+
+<script>
+    import uniqueId from "lodash.uniqueid";
+
+    export default {
+        name: 'App',
+        components: {
+        },
+        data() {
+            return {
+                ToDoItems: [
+                    {id: uniqueId("todo-"), label: 'Learn Vue', done: false},
+                    {id: uniqueId("todo-"), label: 'Create a Vue project with the CLI', done: true},
+                    {id: uniqueId("todo-"), label: 'Have fun', done: true},
+                    {id: uniqueId("todo-"), label: 'Create a to-do list', done: false}
+                ]
+            }
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+createApp(App).mount('#app')
