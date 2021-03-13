3f06b6cbf0e330be777c307219c5bd7d09670c5a
Manuel <merodriguez9112@gmail.com>
2021-03-13 10:50:31 -0500

    import and export tool

--- src/repositories/storageRepository.js
+++ src/repositories/storageRepository.js
@@ -7,5 +7,21 @@ export default {
     },
     remove(key) {
         localStorage.removeItem(key);
+    },
+    clean(){
+        localStorage.clear();
+    },
+    as_json(){
+        var values = {}, keys = Object.keys(localStorage), i = keys.length;
+        while (i--) {
+            values[keys[i]] = localStorage.getItem(keys[i]);
+        }
+        return JSON.stringify(values);
+    },
+    load_json(data){
+        var keys = Object.keys(data), i = keys.length;
+        while (i--) {
+            localStorage.setItem(keys[i],data[keys[i]]);
+        }
     }
 };
--- src/views/configModal.vue
+++ src/views/configModal.vue
@@ -8,25 +8,41 @@
           <i class="bi-x close-modal" data-bs-dismiss="modal"></i>
         </div>
         <div class="modal-body">
-          <div class="mb-5">
-            <label for="language" class="form-label">{{ $t("settings.language") }}</label>
+          <div class="mb-4">
+            <label for="language" class="form-label">{{ $t("settings.language") }}:</label>
             <select id="language" class="col-sm-9 form-select" aria-label="Default select example" v-model="language"
                     @change="changeLanguage">
               <option value="en">English</option>
               <option value="es">Español</option>
             </select>
           </div>
-          <div class="d-flex">
-            <div class="form-check form-switch flex-fill">
+          <div class="mb-3">
+            <div class="form-check form-switch">
               <input class="form-check-input" type="checkbox" id="customListsSetting" v-model="customList"
                      @change="changeCustomList">
               <label class="form-check-label" for="customListsSetting">{{ $t("settings.customLists") }}</label>
             </div>
-            <div class="form-check form-switch flex-fill">
+          </div>
+          <div class="mb-4">
+            <div class="form-check form-switch">
               <input class="form-check-input" type="checkbox" id="darkThemeSetting" v-model="darkTheme"
                      @change="changeDarkTheme">
               <label class="form-check-label" for="darkThemeSetting">{{ $t("settings.darkTheme") }}</label>
             </div>
+          </div>
+          <div class="horizontal-divider mb-4"></div>
+          <div>
+            <label for="language" class="form-label">Copia de Seguridad:</label>
+            <div class="modal-footer d-flex">
+              <button type="button" class="btn flex-fill" @click="exportData"><i
+                class="icons bi-cloud-arrow-down-fill"></i> Exportar
+              </button>
+              <button type="button" class="btn flex-fill" @click="$refs.loadData.click"><i
+                class="icons bi-cloud-arrow-up-fill"></i> Importar
+              </button>
+            </div>
+            <input type="file" id="file-selector" class="d-none" accept=".wtdb" ref="loadData"
+                   @change="importData($event)">
           </div>
         </div>
       </div>
@@ -36,6 +52,7 @@
 
 <script>
     import configRepository from "../repositories/configRepository";
+    import storageRepository from "../repositories/storageRepository";
 
     export default {
         name: "configModal",
@@ -65,6 +82,31 @@
                     configRepository.update(this.$store.state.config);
                     this.$i18n.locale = this.language;
                 });
+            },
+            exportData: function () {
+                var filename = "WeekToDoBackup.wtdb";
+                var fileBody = storageRepository.as_json();
+
+                var element = document.createElement('a');
+                element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(fileBody));
+                element.setAttribute('download', filename);
+                element.style.display = 'none';
+                document.body.appendChild(element);
+                element.click();
+                document.body.removeChild(element);
+            },
+            importData: function (event) {
+                const fileList = event.target.files;
+                if (fileList[0]) {
+                    var fr = new FileReader();
+                    fr.readAsText(fileList[0]);
+                    fr.onload = function () {
+                        var data = JSON.parse(fr.result);
+                        storageRepository.clean();
+                        storageRepository.load_json(data);
+                        location.reload();
+                    }
+                }
             }
         }
     }
@@ -74,6 +116,11 @@
   .form-check-input {
     width: 2.8em !important;
     height: 1.4em !important;
+  }
+
+  .icons {
+    font-size: 20px;
+    margin-right: 5px;
   }
 
   .form-check-label {
@@ -90,5 +137,10 @@
   .form-select:focus {
     box-shadow: none;
   }
+
+  .modal-dialog {
+    max-width: 350px;
+  }
+
 
 </style>
