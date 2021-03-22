90863d5fa08fe27a8e0114c9b960be43c0eb340a
Manuel <merodriguez9112@gmail.com>
2021-03-22 15:23:47 -0400

    adding languages

--- src/assets/languages/languages.js
+++ src/assets/languages/languages.js
@@ -1,9 +1,15 @@
 import en from './en.json'
 import es from './es.json'
+import ru from './ru.json'
+import fr from './fr.json'
+import pt from './pt.json'
 
 export const defaultLocale = 'en'
 
 export const languages = {
     en: en,
     es: es,
+    ru: ru,
+    fr: fr,
+    pt: pt,
 }
