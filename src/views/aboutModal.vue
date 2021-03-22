4f6bf736e92fbddb548e703391b9c5559e42dc3e
Manuel <merodriguez9112@gmail.com>
2021-03-22 14:38:26 -0400

    resposive warning

--- src/views/aboutModal.vue
+++ src/views/aboutModal.vue
@@ -17,8 +17,8 @@
             </div>
             <div class="col-md-9" style="padding-left: 40px;">
               <div style="margin-top: 10px">
-                <span>{{$t('about.desc')}} <a href="#"><b>{{$t('about.site')}} <i
-                  class="bi-globe2"></i></b></a></span>
+                <span>{{$t('about.desc')}}<b class="visit-site" @click="openWebSite()">{{$t('about.site')}} <i
+                  class="bi-globe2"></i></b></span>
                 <div class="horizontal-divider mt-3 mb-3"></div>
                 <div class="row">
                   <div class="col-md-3">
@@ -27,12 +27,12 @@
                   </div>
                   <div class="col-md-8 pt-3">
                     <span>{{$t('about.dev')}}</span>
-                    <h6 style="margin-top: 5px">Manuel Ernesto García</h6>
+                    <h6 style="margin-top: 5px">{{$t('about.devName')}}</h6>
                   </div>
                 </div>
                 <div class="d-flex" style="margin-top: 30px">
                   <span style="margin-top: 10px; margin-right: 10px;">{{$t('about.madeWith')}} <i class="bi-heart-fill"
-                                                                                   style="color: red"></i> {{$t('about.inCuba')}} 2021</span>
+                                                                                                  style="color: red"></i> {{$t('about.inCuba')}} 2021</span>
                   <button type="button" class="btn flex-fill" data-bs-dismiss="modal" data-bs-toggle="modal"
                           data-bs-target="#donateModal">{{$t('donate.donate')}}
                   </button>
@@ -56,7 +56,16 @@
                 version: version_json.version
             }
         },
-        methods: {}
+        methods: {
+            openWebSite: function () {
+                let isElectron = require("is-electron");
+                if (isElectron()) {
+                    require('electron').shell.openExternal('https://weektodo.netlify.app', '_blank');
+                } else {
+                    window.open('https://weektodo.netlify.app', '_blank');
+                }
+            }
+        }
     }
 </script>
 
@@ -67,6 +76,10 @@
 
   a, a:hover {
     color: unset;
+  }
+
+  .visit-site{
+    cursor: pointer;
   }
 
 </style>
