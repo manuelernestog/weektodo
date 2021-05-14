1f299f92595559e8f84b955fd0045fe588360cf1
Manuel <merodriguez9112@gmail.com>
2021-05-14 12:02:15 -0400

    fixing error in germany language

--- src/views/configModal.vue
+++ src/views/configModal.vue
@@ -15,8 +15,8 @@
               <option value="en">English</option>
               <option value="es">Español</option>
               <option value="fr">Français</option>
-              <option value="pt">Português</option>
               <option value="de">Deutsche</option>
+              <option value="pt">Português</option>
               <option value="ru">русский</option>
               <option value="zh_cn">中國人</option>
             </select>
--- src/views/welcome/languageView.vue
+++ src/views/welcome/languageView.vue
@@ -17,6 +17,7 @@
             <option value="en">English</option>
             <option value="es">Español</option>
             <option value="fr">Français</option>
+            <option value="de">Deutsche</option>
             <option value="pt">Português</option>
             <option value="ru">русский</option>
             <option value="zh_cn">中國人</option>
