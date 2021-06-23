09869b1df014c488363713a3666e8784e4515f56
Manuel <merodriguez9112@gmail.com>
2021-06-23 14:28:41 -0400

    Using yarn again

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 if [ "$TRAVIS_OS_NAME" == osx ]; then
-    npm run electron:build -p always
+    yarn electron:build -p always
 else
-    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "npm run electron:build -p always"
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn electron:build -p always"
 fi
