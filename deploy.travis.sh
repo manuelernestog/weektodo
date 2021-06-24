73c3fba619fa1f6f502523cdcfd44013e25c72ec
Manuel <merodriguez9112@gmail.com>
2021-06-24 14:30:59 -0400

    removing yarn lock

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -2,5 +2,5 @@
 if [ "$TRAVIS_OS_NAME" == osx ]; then
     npm run electron:build -- -p always
 else
-    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "npm run electron:build -- -p always"
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "npm run electron:build -- --linux deb rpm snap --win nsis"
 fi
