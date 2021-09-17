c840f34cc40df4c431c53d04d02f569834b5e1f4
Manuel <merodriguez9112@gmail.com>
2021-09-17 11:54:31 -0400

    updating data iniatilization helper

--- src/helpers/initialDataCreator.js
+++ src/helpers/initialDataCreator.js
@@ -16,6 +16,12 @@ export default {
         create_todo(vue, today_id, vue.$t("generatedData.data4"), false);
         create_todo(vue, tomorrow_id, vue.$t("generatedData.data5"), false);
 
+        let subtasks = [
+            {text: vue.$t("generatedData.subtask1"), checked: false, editing: false},
+            {text: vue.$t("generatedData.subtask2"), checked: false, editing: false},
+        ];
+        create_todo(vue, tomorrow_id, vue.$t("generatedData.data11"), false,vue.$t("generatedData.desc1"), subtasks);
+
         create_todo(vue, custom_list1_id, vue.$t("generatedData.data6"), false);
         create_todo(vue, custom_list1_id, vue.$t("generatedData.data7"), false);
         create_todo(vue, custom_list1_id, vue.$t("generatedData.data8"), false);
@@ -24,8 +30,20 @@ export default {
     }
 };
 
-function create_todo(vue, list_id, text, checked) {
-    var newTodo = {text: text, checked: checked, listId: list_id};
+function create_todo(vue, list_id, text, checked, desc = "", subTaskList = [], color = "none") {
+    var newTodo = {
+        text: text,
+        checked: checked,
+        listId: list_id,
+        desc: desc,
+        subTaskList: subTaskList,
+        color: color,
+        priority: 0,
+        tags: [],
+        time: null,
+        alarm: false,
+        repeatingEvent: null
+    }
     vue.$store.commit('addTodo', newTodo);
     toDoListRepository.update(list_id, vue.$store.getters.todoLists[list_id]);
 }
