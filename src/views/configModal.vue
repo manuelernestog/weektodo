d71026349903895f1bc6d4f9c45ad8b46531a3db
Mohamed Mansour <mohmans@pm.me>
2023-02-23 00:43:48 +0300

    Fix Arabic name in the language selection

--- src/views/configModal.vue
+++ src/views/configModal.vue
@@ -293,7 +293,7 @@
                   <option value="ru">русский</option>
                   <option value="ja">日本</option>
                   <option value="pl">Polski</option>
-                  <option value="ar">عرب</option>
+                  <option value="ar">العربية</option>
                   <option value="ko">한국어</option>
                   <option value="zh_cn">简体中文</option>
                   <option value="zh_tw">繁體中文</option>
