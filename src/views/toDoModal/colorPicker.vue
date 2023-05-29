dceb2dd289065f83bff6eb36f1de84e297d85377
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-05-29 11:32:49 -0400

    adding new colors to color picker

--- src/views/toDoModal/colorPicker.vue
+++ src/views/toDoModal/colorPicker.vue
@@ -8,30 +8,30 @@
         <i class="bi-circle colors-menu-icons" type="button" @click="selectColor('none')"></i>
         <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#77e785')"
           style="color: #77e785"></i>
-        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#ed544b')"
-          style="color: #ed544b"></i>
-        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#ed544b')"
-          style="color: #ed544b"></i>
-      </div>
-      <div>
+        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#06b6d4')"
+          style="color: #06b6d4"></i>
         <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#5e6ef2')"
           style="color: #5e6ef2"></i>
-        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#f9d54a')"
-          style="color: #f9d54a"></i>
+      </div>
+      <div>
+        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#8b5cf6')"
+          style="color: #8b5cf6"></i>
         <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#ed56a1')"
           style="color: #ed56a1"></i>
         <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#ed544b')"
           style="color: #ed544b"></i>
+        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#f97316')"
+          style="color: #f97316"></i>
       </div>
       <div>
-        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#5e6ef2')"
-          style="color: #5e6ef2"></i>
         <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#f9d54a')"
           style="color: #f9d54a"></i>
-        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#ed56a1')"
-          style="color: #ed56a1"></i>
-        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#ed544b')"
-          style="color: #ed544b"></i>
+        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#ba7956')"
+          style="color: #ba7956"></i>
+        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#6b7280')"
+          style="color: #6b7280"></i>
+        <i class="bi-circle-fill colors-menu-icons" type="button" @click="selectColor('#030712')"
+          style="color: #030712"></i>
       </div>
 
     </ul>
