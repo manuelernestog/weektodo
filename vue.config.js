2c786d5b4250fc209bf5ff729e1efdc08bd0c557
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-05-29 12:44:09 -0400

    upgrading all packages and dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,35 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = {
-    pluginOptions: {
-        electronBuilder: {
-            nodeIntegration: true,
-            builderOptions: {
-                appId: "weektodo-app.netlify.app",
-                productName: "WeekToDo",
-                publish: ['github'],
-                linux: {
-                    category: "Utility",
-                    description: "Free and Open Source Minimalist Weekly Planner and To Do list App focused on privacy.",
-                    target: ["deb", "rpm","pacman","AppImage"],
-                    icon: "build/icon.icns"
-                },
-                win: {
-                    target: ["nsis"],
-                },
-                mac: {
-                    category: "public.app-category.productivity",
-                    target: ["dmg","pkg"]
-                }
-            }
-        }
-    }
-}
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        appId: "weektodo-app.netlify.app",
+        productName: "WeekToDo",
+        publish: ["github"],
+        linux: {
+          category: "Utility",
+          description: "Free and Open Source Minimalist Weekly Planner and To Do list App focused on privacy.",
+          target: ["deb", "rpm", "pacman", "AppImage"],
+          icon: "build/icon.icns",
+        },
+        win: {
+          target: ["nsis"],
+        },
+        mac: {
+          category: "public.app-category.productivity",
+          target: ["dmg", "pkg"],
+        },
+      },
+    },
+  },
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
+    resolve: {
+      fallback: {
+        fs: false,
+      },
+    },
+  },
+};
