4352657487bcf6cd1edab4c837cc50d5d26bc1b1
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-06-01 10:42:20 -0400

    tasting again with other config

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -3,7 +3,7 @@ if [ "$TRAVIS_OS_NAME" == osx ]; then
   yarn electron:build -p always
 else
   sudo apt-get install libarchive-tools
-  sudo apt-get install bsdtar
+  # sudo apt-get install bsdtar
   sudo apt-get update -qq
   sudo apt-get install snapd
   sudo snap install snapcraft --classic
