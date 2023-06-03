034e70c684ba6ff4af5fe4c06029e146c22858bb
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-06-03 09:59:53 -0400

    moving back to old dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'eslint:recommended'
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
