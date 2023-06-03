0dc42401bdebee18c42423ded80450f2e4720ff3
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-06-03 16:14:01 -0400

    updating travis deploy config

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -7,6 +7,5 @@ else
   sudo apt-get update -qq
   sudo apt-get install snapd
   sudo snap install snapcraft --classic
-  echo $SNAP_TOKEN | snapcraft login --with -
   yarn electron:build --linux snap -p always
 fi
